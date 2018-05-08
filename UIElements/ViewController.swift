fe10d5fc655817617888cd6213acff0b3ca1b01d
Алексей Ефимов <lex.efimov@gmail.com>
2018-05-08 16:51:55 +0500

    UISegmentedControl UISlider

--- UIElements/ViewController.swift
+++ UIElements/ViewController.swift
@@ -2,7 +2,7 @@
 //  ViewController.swift
 //  UIElements
 //
-//  Created by Debash on 08.05.2018.
+//  Created by Debash on 07.05.2018.
 //  Copyright © 2018 swiftbook.ru. All rights reserved.
 //
 
@@ -10,16 +10,53 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var segmentedControl: UISegmentedControl!
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var slider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        slider.value = 1
+        
+        label.text = String(slider.value)
+        label.font = label.font.withSize(35)
+        label.textAlignment = .center
+        label.numberOfLines = 2
+        
+        segmentedControl.insertSegment(withTitle: "Third", at: 2, animated: true)
+        
+        slider.minimumValue = 0
+        slider.maximumValue = 1
+        slider.minimumTrackTintColor = .yellow
+        slider.maximumTrackTintColor = .red
+        slider.thumbTintColor = .blue
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func choiceSegment(_ sender: UISegmentedControl) {
+        
+        label.isHidden = false
+        
+        switch segmentedControl.selectedSegmentIndex {
+        case 0:
+            label.text = "The first segment is selected"
+            label.textColor = .red
+        case 1:
+            label.text = "The second segment is selected"
+            label.textColor = .blue
+        case 2:
+            label.text = "The third segment is selected"
+            label.textColor = .yellow
+        default:
+            print("Sothing wrong!")
+        }
     }
-
-
+    
+    @IBAction func sliderAction(_ sender: UISlider) {
+        label.text = String(sender.value)
+        
+        let backgrandColor = self.view.backgroundColor
+        self.view.backgroundColor = backgrandColor?.withAlphaComponent(CGFloat(sender.value))
+    }
 }
 
