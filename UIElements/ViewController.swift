efb13e633cbdc90e85573da6af11109f2e1e19f7
Алексей Ефимов <lex.efimov@gmail.com>
2018-05-08 13:29:45 +0500

    Initial Commit

---
+++ UIElements/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UIElements
+//
+//  Created by Debash on 08.05.2018.
+//  Copyright © 2018 swiftbook.ru. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
